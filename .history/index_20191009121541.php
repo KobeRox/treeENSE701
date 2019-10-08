842e631196bd97ef596348193c3241172174c3c9
KobeRox <888888xc@gmail.com>
2019-10-09 12:20:29 +1300

    make it better

---
+++ .history/index_20191009121541.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>PlantATree</title>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+        <title>PlantATree</title>
+        <!-- this is some changes -->
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+        <!-- Bootstrap core CSS -->
+        <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="/src/CSS/album.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <nav class="navbar navbar-expand-lg" style="background-color: rgb(96, 114, 62)">
+            <div class="container">
+                <a class="navbar-brand text-light" href="#">PlantATree</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+                       <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link text-light" href="index.php">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-light" href="treeDetails.html">Tree Details</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-light" href="productPage.html">Products</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-light" href="aboutPage.html">About</a>
+                            </li>
+                        </ul>
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="cart.html">Shopping Cart</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+      
+        <div style="height: 250px">
+            <img src="/src/images/nature.jpg" alt="" style="height: 100%; width: 100%; object-fit: cover">
+            <div class="centered text-light"><h1>PlantATree</h1></div>
+        </div>
+        <div class="container">
+          <div>
+            <br/>
+            <h2 class="text-center">TreeCo</h2>
+            <br/>
+              <p>TreeCo, A company based in New Zealand that specializes at Tree sales. They sell trees at different stages for any projects from small to large.
+                TreeCo developed a web application named PlantATree which enables customers to buy their products from anywhere in New Zealand through the use of their mobile phones.
+              </p>
+            <br/>
+              <p>
+                TreeCo values their loyal customers. They are respected and looked after through some special deals that they can claim at the checkout or the shopping cart. 
+                Aside from selling different types of Trees. TreeCo also enables the purchase of gardening tools and suggested information that helps the buyer to maintain their Trees and Plants alive.
+              </p>
+          </div>  
+        </div>
+        <br/>
+    </body>
+
+    <!-- Footer -->
+    <footer class="page-footer font-small blue" style="background-color: rgb(96, 114, 62)">
+        <div class="footer-copyright text-center py-3">© 2018 Copyright:
+          <a href="" class="text-light"> PlantATree</a>
+        </div>      
+    </footer>
+</html>
